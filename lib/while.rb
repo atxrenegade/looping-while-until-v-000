a893f38ad52c6d9cbe6acbd6649a35e419ec5d19
Harleigh Abel <harleighabel@gmail.com>
2018-01-26 22:18:21 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,5 +1,5 @@
-def using_until 
-  count = 0
+def using_until
+  count = 6
   until count = 10
     puts "Wingardium Leviosa"
     count += 1
@@ -7,7 +7,7 @@ def using_until
 end
 
 def using_while
-  count = 0
+  count = 6
   while count < 10
     puts "Wingardium Leviosa"
     count += 1
