2f6c84a4f220f282ede5b7096109360012d782f0
Harleigh Abel <harleighabel@gmail.com>
2018-01-26 22:17:41 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,1 +1,15 @@
+def using_until 
+  count = 0
+  until count = 10
+    puts "Wingardium Leviosa"
+    count += 1
+  end
+end
 
+def using_while
+  count = 0
+  while count < 10
+    puts "Wingardium Leviosa"
+    count += 1
+  end
+end  
